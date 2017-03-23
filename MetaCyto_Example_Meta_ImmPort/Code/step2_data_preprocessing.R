b115bf693c079e6fc8e496bcb559fa791da812fe
hzc363 <hzc363@gmail.com>
2017-03-23 14:33:49 -0700

    updated examples

--- MetaCyto_Example_Meta_ImmPort/Code/step2_data_preprocessing.R
+++ MetaCyto_Example_Meta_ImmPort/Code/step2_data_preprocessing.R
@@ -26,4 +26,36 @@ preprocessing.batch(inputMeta=fcs_info,
                     outpath="Result/preprocess_output",
                     excludeTransformParameters=c("FSC-A","FSC-W","FSC-H","Time","Cell_length"))
 
+#########################################
+########## Examine the Panels ##########
+########################################
+
+# collect data output from the step1. 
+files=list.files("Result",pattern="processed_sample",recursive=T,full.names=T)
+panel_info=collectData(files,longform=F)
+
+# summarize markers in all markers. The panel summary is written to "Result" Folder
+PS=panelSummary(panel_info,"Result",cluster=F,width=30,height=20) 
+
+
+###################################
+########## Updata Names ##########
+##################################
+
+# CD8 is called "CD8B" in one study and is called "CD8" in another. 
+# Here, we change CD8B to CD8 for consistency.
+ab_names=sort(rownames(PS))
+nameUpdator("CD8B","CD8",files)
+
+
+#####################################################
+########## Examine the Panels after update ##########
+#####################################################
+panel_info=collectData(files,longform=F)
+PS=panelSummary(panel_info,"Result",cluster=F,width=30,height=20)
+
+
+
+
+
 
