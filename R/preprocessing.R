8b8a7a4be44bed5789fda8fbf446e0d95a7bf14d
hzc363 <hzc363@gmail.com>
2018-07-31 10:29:32 -0700

    update preprocessing function

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -74,7 +74,7 @@ preprocessing=function(fcsFiles,
       trans = flowCore::logTransform()
       translist = flowCore::transformList(biomarker_vector,trans)
       fcs = flowCore::transform(fcs, translist)
-    } else if (unique_version == 3) {
+    } else if (unique_version >= 3) {
       # check if user have provided the compmatrix
       if(!is.null(compFiles)){
         compList=lapply(compFiles,function(x){
